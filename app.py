28ff2b1f22f26b4b368baa30c547afb9cc098a23
Kiran Kumar G <96608447+itz-kirankumar@users.noreply.github.com>
2023-09-04 10:39:26 +0530

    Development Update

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from ultralytics import YOLO
-# import pygame
-
-# Initialize pygame mixer
-# pygame.mixer.init()
 
 # Load the pre-trained YOLOv8 model using Ultralytics
 model = YOLO('best.pt')
@@ -25,37 +21,22 @@ st.sidebar.title('Settings')
 confidence_threshold = st.sidebar.slider('Confidence Threshold', min_value=0.0, max_value=1.0, value=0.5)
 override_threshold = st.sidebar.slider('Override Threshold', min_value=0.0, max_value=1.0, value=0.5)
 
-# Alert sound file
-# alert_sound = 'alert.wav'
-
 # Main content
-use_webcam = st.checkbox('Use Webcam')
-use_ipcam = st.checkbox('Use IP cam')
-if use_ipcam:
-    st.write("IP Camera Stream:")
-    st.write("Under Development For Global Deployment")
-    stframe = st.empty()
-    cap = cv2.VideoCapture("http://100.72.6.11:8080/video")
-elif use_webcam:
-    st.write("Webcam Detection:")
-    st.write("Under Development For Global Deployment")
-    stframe = st.empty()
-    cap = cv2.VideoCapture(0)
-else:
-    st.write("Upload a video for detection:")
-    uploaded_file = st.file_uploader("Choose a file...", type=["mp4", "mov", "avi"])
-    stframe = st.empty()
-    cap = None
-    if uploaded_file is not None:
-        file_extension = uploaded_file.name.split('.')[-1]
-        if file_extension in ['jpg', 'png']:
-            image = np.array(uploaded_file)
-            stframe.image(image, channels="RGB", use_column_width=True)
-        elif file_extension in ['mp4', 'mov', 'avi']:
-            # Save the uploaded video to a temporary file
-            with open('temp_video.mp4', 'wb') as temp_file:
-                temp_file.write(uploaded_file.read())
-            cap = cv2.VideoCapture('temp_video.mp4')
+st.write("Upload a video for detection:")
+uploaded_file = st.file_uploader("Choose a file...", type=["mp4", "mov", "avi"])
+stframe = st.empty()
+cap = None
+
+if uploaded_file is not None:
+    file_extension = uploaded_file.name.split('.')[-1]
+    if file_extension in ['jpg', 'png']:
+        image = np.array(uploaded_file)
+        stframe.image(image, channels="RGB", use_column_width=True)
+    elif file_extension in ['mp4', 'mov', 'avi']:
+        # Save the uploaded video to a temporary file
+        with open('temp_video.mp4', 'wb') as temp_file:
+            temp_file.write(uploaded_file.read())
+        cap = cv2.VideoCapture('temp_video.mp4')
 
 class_list = ["bear", "cat", "cheetah", "cow", "dog", "elephant", "fire", "goat", "hen", "horse", "human", "lion",
               "monkey", "panda", "rhino", "tiger", "zebra"]
@@ -85,10 +66,6 @@ while cap is not None and cap.isOpened():
                 if row[4] >= override_threshold:
                     # Trigger an alert mechanism (print a message for demonstration)
                     st.warning(f"ALERT: Object Detected - {c}")
-                    # Play alert sound using pygame
-                    # pygame.mixer.music.load(alert_sound)
-                    # pygame.mixer.music.play()
-                    # alert_triggered = True
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         stframe.image(frame, channels='RGB', use_column_width=True)
