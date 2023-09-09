54a3241caa90429e81958258797ccc220ca94cbb
Kiran Kumar G <96608447+itz-kirankumar@users.noreply.github.com>
2023-09-09 09:19:07 +0530

    setup update

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 # Install system dependencies
 apt-get update -y
 apt-get install -y libgl1-mesa-glx
+apt-get install libsdl1.2-dev libsdl-image1.2 libsdl-mixer1.2 libsdl-ttf2.0
 pip install streamlit --upgrade
